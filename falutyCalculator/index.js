24ddef3bc7d3c152ebd83ad1ff2e542989f273d6
JatinThakur-797 <jt6285638@gmail.com>
2025-09-19 20:33:19 +0530

    Now we start learning of JavaScript

---
+++ falutyCalculator/index.js
@@ -0,0 +1,15 @@
+console.log("hey i am jatin");
+
+let a = 5 ;
+let b = 6;
+let c = '/';
+
+let rand = Math.random();
+
+if(rand<0.1){
+    
+    console.log(a**b);
+}
+else{
+    console.log(a/b);
+}
---
+++ js variables/script.js
@@ -0,0 +1,22 @@
+console.log("hey this tutorial of js")
+
+var a = 5; // var is a global variable
+let b = 3; // let is local variable
+var c = "jatin";
+var _a = "Thakur";
+// var 55a = "Rohan" Its invalid
+const a1 = 3; // not allowed to change its value
+
+console.log(a+b+8)
+console.log(typeof a , typeof b , typeof c)
+{
+    let a = 5 ;
+    console.log(a);
+}
+let o = {
+    "name": "jatin",
+    "job code": 5600
+}
+console.log(o);
+o.salary = "100 crores";
+console.log(o);
