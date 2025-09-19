b8b09ef2357e2f2a0feb0f7027256e14ef77787f
JatinThakur-797 <jt6285638@gmail.com>
2025-09-19 20:58:15 +0530

    learning of Loops in js

---
+++ js Loops/loops.js
@@ -0,0 +1,17 @@
+console.log("Welcome to learning of loops");
+//for loop
+for(let i = 0 ; i< 5 ; i++){
+    console.log("hello");
+}
+//while loop
+var i = 0;
+while(i<5){
+    console.log("World");
+    i++;
+}
+//do while loop
+var i = 0;
+do{
+    console.log("jatin");
+    i++;
+}while(i<5)
