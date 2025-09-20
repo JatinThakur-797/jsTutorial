8e802a062ef43fcd7caeba226a9d844791947216
JatinThakur-797 <jt6285638@gmail.com>
2025-09-20 21:05:09 +0530

    learning for js Functions

---
+++ js functions/functions.js
@@ -0,0 +1,18 @@
+console.log("Welcome to tutorial of functions");
+
+var a = 5;
+var b = 6;
+
+function add(a, b){
+    return a+b;
+}
+
+var firstName = "Jatin ";
+var secondName = "Thakur";
+
+function giveName(fname , lname){
+    return fname + lname;
+}
+
+console.log(add(a,b));
+console.log(giveName(firstName,secondName));
