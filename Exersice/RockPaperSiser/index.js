ea70f2bb42872278a98e0bcb5c8a92bb3dee0153
JatinThakur-797 <jt6285638@gmail.com>
2025-10-07 10:43:28 +0530

    Learning Js Feature

---
+++ Exersice/RockPaperSiser/index.js
@@ -0,0 +1,2 @@
+let b = 'rock';
+console.log(`The image path is: img/${b}.png`);
--- js variables/script.js
+++ js variables/script.js
@@ -15,8 +15,53 @@ console.log(typeof a , typeof b , typeof c)
 }
 let o = {
     "name": "jatin",
-    "job code": 5600
+    "job code": 5600,
+    "batch" : "Cs"
 }
 console.log(o);
 o.salary = "100 crores";
+o.add = "Malti Road";
 console.log(o);
+
+if(a<=5){
+    let b = 10;
+    console.log(b);
+}
+console.log(b);
+
+// let d = 5;
+// let e = 5;
+// if(d===e){
+//     console.log("success");
+// }else{
+//     console.log("not success");
+// }
+//     console.log(eval('45+9+90'));
+
+//     let p = "Hello World!";
+//     let n = p.substring(0, p.length - 1);
+
+//     console.log(n);
+//     p = n;
+//     console.log(p)
+//     for(let i = 0 ; i< 10 ; i++){
+
+//          let ran = Math.random() *10;
+//         let computer = "";
+//         if(ran >= 0 && ran <= 3 ){
+//             computer = "rock";
+//         }else if(ran >3 && ran <7 ){
+//             computer = "paper";
+//         }else{
+//             computer = "scisser";
+//         }
+//         console.log(computer)
+//     }
+
+
+let bo = 3;
+function test(){
+    bo = 5;
+}    
+test();
+console.log(bo);
