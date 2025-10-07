83fbb9f896db6a3de66bfc63b3809c53551266f4
JatinThakur-797 <jt6285638@gmail.com>
2025-10-07 13:02:49 +0530

    learning JSON Object

---
+++ js JSONObject/script.js
@@ -0,0 +1,16 @@
+let ob = {
+    name : 'sam',
+    age : '18',
+    city : 'Dhar',
+    street : 'Malti Road',
+    Cast : 'Gen',
+
+
+}
+console.log(ob);
+let str = JSON.stringify(ob);
+console.log(str);
+console.log(JSON.parse(str));
+
+
+
