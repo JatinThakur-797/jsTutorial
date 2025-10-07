cfe43e6c223c40d28da88ad51bd3125c1b11da07
JatinThakur-797 <jt6285638@gmail.com>
2025-10-07 10:53:46 +0530

    learning if_alternatives

---
+++ if_alternatives/script.js
@@ -0,0 +1,17 @@
+
+let age = 18;
+//Ternary Operator
+let result = age>18 ? 'adult' :'child';
+console.log(result); 
+
+//Gauard Operator
+ age = null  ;  
+ let finalAge = age || 19;
+ console.log(finalAge);
+ 
+
+ //Default Operator
+
+ age = undefined ;
+finalAge = age ?? 19;
+console.log(finalAge);
