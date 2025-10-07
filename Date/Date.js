c183373eebb0667361940429dcba83647014333b
JatinThakur-797 <jt6285638@gmail.com>
2025-10-07 14:17:41 +0530

    Learnig Date

---
+++ Date/Date.js
@@ -0,0 +1,9 @@
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
+console.log(myDate.getDate());
+console.log(myDate.getHours());
